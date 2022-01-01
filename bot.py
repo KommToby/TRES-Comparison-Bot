483e9a74957920ab1fd7658a1e17a29dfe64d616
Toby <kommandertoby@gmail.com>
2022-01-01 15:04:09 +0000

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,40 @@
+# TODO
+
+import json
+import os
+
+from discord.ext import commands
+
+
+with open("config.json") as f:
+    DISCORD_CONFIG_DATA = json.load(f)["discord"]
+    TOKEN = DISCORD_CONFIG_DATA["token"]
+
+GUILD = None
+client = commands.Bot(command_prefix="-")
+
+# called when bot is online
+@client.event
+async def on_ready():
+    print("Connected")
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            client.load_extension(f"cogs.{filename[:-3]}")
+
+
+@client.command(name="load")
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(f"Loaded {extension}")
+
+
+# Command unloader
+@client.command(name="unload")
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f"Unloaded {extension}")
+
+
+#  must be final line
+if __name__ == '__main__':
+    client.run(TOKEN)
